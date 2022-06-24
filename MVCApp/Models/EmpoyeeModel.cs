99fc3e25f0e4f92e575d16e7182aa16245b9311e
Sabir Aliyev <sabir.aliyev.itdep@gmail.com>
2022-06-24 10:23:33 +0400

    Add project files.

---
+++ MVCApp/Models/EmpoyeeModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MVCApp.Models
+{
+    public class EmpoyeeModel
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int EmailAddress { get; set; }
+        public int ConfirmEmail { get; set; }
+        public int Password { get; set; }
+        public int ConfirmPassword { get; set; }
+    }
+}
